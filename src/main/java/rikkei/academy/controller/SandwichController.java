3f12a15d3900e0098a79f8de2104d5e0a02b3e1b
bangng <montersb@gmail.com>
2022-10-17 14:27:45 +0700

    up

---
+++ src/main/java/rikkei/academy/controller/SandwichController.java
@@ -0,0 +1,22 @@
+package rikkei.academy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class SandwichController {
+    @RequestMapping("")
+    public String viewIndex(){
+        return "index";
+    }
+    @RequestMapping("/condiments")
+    public ModelAndView save(@RequestParam String condiment){
+        ModelAndView modelAndView = new ModelAndView("save");
+        modelAndView.addObject("list",condiment);
+        return modelAndView;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/save.jsp
@@ -0,0 +1,22 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 10/17/2022
+  Time: 2:04 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List Condiments are chosen</title>
+</head>
+<body>
+<h1>List Condiments are chosen</h1>
+<c:forEach var="cd" items="${list}">
+    <h2>${cd.condiment}</h2>
+</c:forEach>
+<a href="/">Back</a>
+
+</body>
+</html>
